e125e3fec65291ab8fbd9b6cd269d4aa6c210220
Obayashi <4210.tatu@gmail.com>
2018-04-21 15:20:21 +0900

    05.プログラミング言語 Java

--- app/src/main/java/com/example/tatunori/firstapp/MainActivity.java
+++ app/src/main/java/com/example/tatunori/firstapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tatunori.firstapp;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("10の階乗");
+        TextView firstText = (TextView)findViewById(R.id.firstText);
+        Resources res = getResources();
+        int color = res.getColor(R.color.colorAccent);
+
+//        long factorial = caluculaterFactorial(20);
+        firstText.setTextSize(100);
+        firstText.setText(String.valueOf(tenOfFactorial()));
+        firstText.setTextColor(color);
+    }
+    private long caluculaterFactorial(int n){
+        long product =1L;
+        for (int j = 1; j <=n ; j++) {
+            product = product * j;
+        }
+        return product;
+    }
+    private int tenOfFactorial(){
+        return 10*10;
     }
 }
